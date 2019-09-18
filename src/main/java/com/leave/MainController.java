9d17bd04d67465aa93bf7e3e1783787446fcba7a
Krishna Kumar Parakkat <krishnakumar.parakkat@cognizant.com>
2019-09-17 19:22:01 -0500

    spring boot project structure

---
+++ src/main/java/com/leave/MainController.java
@@ -0,0 +1,28 @@
+package com.leave;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping("/add/{firstno}/{secondno}")
+    public String addition(@PathVariable String firstno, @PathVariable String secondno) {
+        int result = Integer.parseInt(firstno)+Integer.parseInt(secondno);
+    	return String.valueOf(result);
+    }
+    
+    @RequestMapping("/subtract/{firstno}/{secondno}")
+    public String subtract(@PathVariable String firstno, @PathVariable String secondno) {
+        int result = Integer.parseInt(firstno)-Integer.parseInt(secondno);
+    	return String.valueOf(result);
+    }
+    
+
+}
